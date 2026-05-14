5f834fb75ad6087e90e2fc6ae6190b327fb51885
Likogeles <kirill.dolgov.73@yandex.ru>
2026-05-14 08:10:35 +0400

    Добавлены настройки пользователя и распределены характеристики мест

---
+++ preferences.py
@@ -0,0 +1,104 @@
+prefs = ["military", "religion", "architecture", "transport", "sight", "interactive", "nutrition", "housing"]
+
+military = [
+    "battlefield"
+    ]
+
+religion = [
+    "cross",
+    "place_of_worship",
+    "church",
+    "wayside_cross",
+    "wayside_shrine"
+    ]
+
+architecture = [
+    "castle",
+    "city_gate",
+    "epigraph",
+    "fountain",
+    "gate",
+    "historic",
+    "hospital",
+    "lean_to",
+    "archaeological_site",
+    "manor",
+    "memorial",
+    "milestone",
+    "ruins",
+    "suburb"
+    ]
+
+transport = [
+    "aircraft",
+    "anchor",
+    "locomotive",
+    "missile",
+    "propeller",
+    "railway_car",
+    "ship",
+    "technical_monument",
+    "train",
+    "vehicle",
+    "wreck",
+    "cannon"
+    ]
+
+sight = [
+    "artwork",
+    "attraction",
+    "bench",
+    "binoculars",
+    "boundary_stone",
+    "clock",
+    "exhibit",
+    "grave",
+    "information",
+    "monument",
+    "printing_press",
+    "shield",
+    "statue",
+    "stone",
+    "tank",
+    "viewpoint"
+    ]
+
+interactive = [
+    "amusement_arcade"
+    "aquarium"
+    "dog_park"
+    "gallery"
+    "library"
+    "mine"
+    "museum"
+    "picnic_site"
+    "picnic_table"
+    "railway_station"
+    "sauna"
+    "social_facility"
+    "swimming_pool"
+    "theme_park"
+    "tomb"
+    "trail_riding_station"
+    "training"
+    "water_park"
+    "zoo"
+    "garden"
+    ]
+
+housing = [
+    "apartment",
+    "camp_site",
+    "caravan_site",
+    "chalet",
+    "dormitory",
+    "guest_house",
+    "hostel",
+    "hotel",
+    "motel"
+    ]
+
+nutrition = [
+    "cafe",
+    "restaurant"
+    ]
---
+++ user.py
@@ -0,0 +1,10 @@
+user_prefs = {
+    "military": 4,
+    "religion": 2,
+    "architecture": 3,
+    "transport": 5,
+    "sight": 4,
+    "interactive": 2,
+    "nutrition": 0,
+    "housing": 0
+    }
